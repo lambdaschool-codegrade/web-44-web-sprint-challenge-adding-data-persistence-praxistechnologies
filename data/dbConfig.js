8ad76ba63bb49f10f9a9d404efe9e094d7081b58
Praxis Technologies <ptanalytics@gmail.com>
2021-08-13 18:58:33 -0400

    code push

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,9 +1,8 @@
 // No need to change this file
 const knex = require('knex');
 const configurations = require('../knexfile.js');
-// const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 
 // What knex configuration is actually used?
 // That depends on the value of process.env.NODE_ENV!
-// module.exports = knex(configurations[environment]);
-module.exports = knex(configurations.development);
+module.exports = knex(configurations[environment]);
--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,19 @@
-// Update with your config settings.
+// do not make changes to this file
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    },
-    useNullAsDefault: true,
-    // generates migration files in a data/migrations/ folder
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
+    ...sharedConfig,
+    connection: { filename: './dev.sqlite3' },
+    seeds: { directory: './data/seeds' },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './dev.sqlite3' },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
