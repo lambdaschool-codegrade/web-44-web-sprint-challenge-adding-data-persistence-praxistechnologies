a3ef03f23e352c5aca8a31575c295217dba14a38
Praxis Technologies <ptanalytics@gmail.com>
2021-08-13 19:52:26 -0400

    code push

--- knexfile.js
+++ knexfile.js
@@ -9,11 +9,11 @@ const sharedConfig = {
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
+    connection: { filename: './data/test.db3' },
     seeds: { directory: './data/seeds' },
   },
   testing: {
     ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
+    connection: { filename: './data/test.db3' },
   },
 };
